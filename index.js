66e04bb6390b1dda1dfa216856549d326b23b26e
Shubham Sinha <49827289+shubhxl@users.noreply.github.com>
2021-01-12 13:37:20 +0530

    v2

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.get('/form', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
+app.get('/card', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  });
 
 const client = new google.auth.JWT(
     keys.client_email,
